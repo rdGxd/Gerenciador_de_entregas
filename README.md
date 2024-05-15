3858e801772061fc36dbe8ab06533e40d0726c5a
Rodrigo <rdg.hora1@gmail.com>
2024-05-15 12:12:36 -0300

    Correios altero a forma com é feita consulta na API (AGORA É NECESSARIO UM CONTRATO)

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Gerenciador_de_entregas
+# Gerenciador_de_entregas (PROJETO PARADO, MOTIVO: OS CORREIOS MUDARAM A API E AGORA É NECESSÁRIO UM CONTRATO)
 
 Crie uma página onde é possível salvar os itens que comprou e demonstrar qual é o local que está o produto. Você pode utilizar as APIs do Correios para pegar essas informações.
--- front-end/src/components/Header/index.tsx
+++ front-end/src/components/Header/index.tsx
@@ -2,40 +2,51 @@
 
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import { ModeToggle } from "../changeTheme";
 
 export default function Header() {
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    setToken(localStorage.getItem("token") as string);
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    window.location.href = "/";
+  };
+
   return (
-    <>
-      <nav className="fixed inset-x-0 top-0 z-50 bg-white shadow-sm dark:bg-gray-950/90">
-        <div className="w-full max-w-7xl mx-auto px-4">
-          <div className="flex justify-around h-14 items-center">
-            <Link className="flex items-center" href="#">
-              <MountainIcon className="h-6 w-6" />
-              <span className="sr-only">Gerenciador de entregas</span>
-            </Link>
-            <div className="flex items-center gap-4">
-              <Link href={"/login"}>
-                <Button size="sm" variant="outline">
-                  Sign in
-                </Button>
-              </Link>
-              <Link href={"/register"}>
+    <nav className="fixed inset-x-0 top-0 z-50 bg-white shadow-sm dark:bg-gray-950/90">
+      <div className="w-full max-w-7xl mx-auto px-4">
+        <div className="flex justify-around h-14 items-center">
+          <Link className="flex items-center" href="#">
+            <MountainIcon />
+            <span className="sr-only">Gerenciador de entregas</span>
+          </Link>
+          <div className="flex items-center gap-4">
+            {token ? (
+              <Button size="sm" variant="outline" onClick={handleLogout}>
+                Logout
+              </Button>
+            ) : (
+              <Link href={"/"}>
                 <Button size="sm">Sign up</Button>
               </Link>
-            </div>
-            <ModeToggle />
+            )}
           </div>
+          <ModeToggle />
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 }
 
-function MountainIcon(props: any) {
+function MountainIcon() {
   return (
     <svg
-      {...props}
+      className="h-6 w-6"
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
